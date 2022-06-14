c7cf026c2b8bf7d7fd7db3722ab4549228e969a9
your Name <mohamedabdulayubm@gmail.com>
2022-06-14 16:12:00 +0530

    Committed

---
+++ FuncClass/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class correctOne
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Class Method to Compute Employee Wage");
+        work check = new work();
+        check.calWage();
+        check.Full();
+    }
+}    
---
+++ FuncClass/Program.cs
@@ -0,0 +1,46 @@
+﻿public class work
+{
+    int isPartTime = 2;
+    int isFullTime = 1;
+    int ratePerHr = 60;
+    int numWorkingDays = 30;
+    int empWrkHrs = 0;
+
+    public void calWage()
+    {
+        Random num = new Random();
+        int ran = num.Next(0, 3);
+
+        if (ran == isPartTime)
+        {
+            Console.WriteLine("Is Part Time");
+            empWrkHrs = 8;
+
+
+        }
+        else if (ran == isFullTime)
+        {
+            Console.WriteLine("Is Present");
+            empWrkHrs = 16;
+
+
+        }
+        else
+        {
+            Console.WriteLine("Is Absent");
+            empWrkHrs = 0;
+        }
+        //int empWage = empWrkHrs * ratePerHr;
+        //int monthlyWage = empWage * numWorkingDays;
+
+        //Console.WriteLine("The Daily salary is :" + empWage+" " + "The Monthly salary is :" + monthlyWage);
+    }
+
+    public void Full()
+    {
+        int empWage = empWrkHrs * ratePerHr;
+        int monthlyWage = empWage * numWorkingDays;
+
+        Console.WriteLine("The Daily salary is :" + empWage+" " + "The Monthly salary is :" + monthlyWage);
+    }
+}
